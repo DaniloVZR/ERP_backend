63c8b22e51bac56d9239de9d01f113733ecb61de
StivenKN <blandon0207s@gmail.com>
2025-02-07 20:14:57 -0500

    autenticacion

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $requestValidated = $request->validate([
+                'email' => 'required|exists:users,email|email',
+                'password' => 'required'
+            ]);
+
+            $user = User::where('email', $requestValidated['email'])->firstOrFail();
+
+            if (!Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'email' => ['The provided credentials are incorrect.'],
+                ]);
+            }
+
+            return response()->json([
+                'token' => $user->createToken('token-name', ['server:update'])->plainTextToken
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'mensaje' => 'Ha ocurrido un error al consultar el usuario',
+                'data' => $th->getMessage(),
+            ], 404);
+        }
+    }
+}
